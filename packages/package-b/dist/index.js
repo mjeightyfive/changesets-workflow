8b39db01bfb0318604993372cb31ef1a32af14b6
mjeightyfive <mjeightyfive@gmail.com>
2020-04-07 09:30:24 +0100

    dont ignore dist

---
+++ packages/package-b/dist/index.js
@@ -0,0 +1,11 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+
+const packageB = () => 'packageB';
+
+var _default = packageB;
+exports.default = _default;
---
+++ packages/package-c/dist/index.js
@@ -0,0 +1,11 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+
+const packageC = () => 'packageC';
+
+var _default = packageC;
+exports.default = _default;
